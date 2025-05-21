14c7de869fe66861c6171866c9dfa3cc6ccb057f
Julio Caro <jcarocota@gmail.com>
2025-05-20 20:24:07 -0700

    Se agregó la entidad Contact

---
+++ app/src/main/java/com/ebc/contactos_app/enity/Contact.kt
@@ -0,0 +1,11 @@
+package com.ebc.contactos_app.enity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contacts")
+data class Contact(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val phone: String
+)

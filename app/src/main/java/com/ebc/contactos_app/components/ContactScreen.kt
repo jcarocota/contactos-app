636570613dc7199db623cb14efe69a2dfe8654c7
Julio Caro <jcarocota@gmail.com>
2025-05-22 20:39:27 -0700

    Iniciamos con la lista de Contactos

--- app/src/main/java/com/ebc/contactos_app/components/ContactScreen.kt
+++ app/src/main/java/com/ebc/contactos_app/components/ContactScreen.kt
@@ -1,8 +1,14 @@
 package com.ebc.contactos_app.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.room.Room
@@ -34,6 +41,37 @@ fun ContactScreen(vm:ContactViewModel, modifier: Modifier = Modifier) {
             label = { Text("Nombre") },
             modifier = Modifier.fillMaxWidth()
         )
+        Spacer(Modifier.height(8.dp))
+        OutlinedTextField(
+            value = phone,
+            onValueChange = {phone = it},
+            label = { Text("Teléfono") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(8.dp))
+        Button(
+            onClick = {
+                vm.addContact(name, phone)
+            }
+        ) {
+            Text("Agregar Contacto")
+        }
+        Spacer(Modifier.height(16.dp))
+
+        LazyColumn {
+            items(contactList) { contact ->
+                Row(
+                    Modifier.fillMaxWidth()
+                        .padding(vertical = 4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(Modifier.weight(1f)) {
+                        Text(text = contact.name)
+                        Text(text = contact.phone)
+                    }
+                }
+            }
+        }
     }
 
 }

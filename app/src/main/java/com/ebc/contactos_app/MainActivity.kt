ecf2ba715c2d44f0d32e56436457c57f0f919fc0
Julio Caro <jcarocota@gmail.com>
2025-05-22 19:45:34 -0700

    Ya creamos la fábrica y el view model desde Main Activity... todo antes de cargar el componente ContactScreen.kt

--- app/src/main/java/com/ebc/contactos_app/MainActivity.kt
+++ app/src/main/java/com/ebc/contactos_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,11 +12,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import com.ebc.contactos_app.room.AppDatabase
 import com.ebc.contactos_app.ui.theme.ContactosappTheme
+import com.ebc.contactos_app.viewmodel.ContactViewModel
+import com.ebc.contactos_app.viewmodel.ContactViewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "contact-db"
+        ).build()
+
+        val dao = db.contactDao()
+
+        val factory = ContactViewModelFactory(dao)
+        val vm: ContactViewModel by viewModels { factory }
+
         enableEdgeToEdge()
         setContent {
             ContactosappTheme {
---
+++ app/src/main/java/com/ebc/contactos_app/components/ContactScreen.kt
@@ -0,0 +1,13 @@
+package com.ebc.contactos_app.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.room.Room
+import com.ebc.contactos_app.viewmodel.ContactViewModelFactory
+
+@Composable
+fun ContacScreen(modifier: Modifier = Modifier) {
+
+
+
+}

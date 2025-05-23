c4975d64a03d95e92604cf86a97b67fe3b75aa9c
Julio Caro <jcarocota@gmail.com>
2025-05-22 18:59:36 -0700

    Agregamos el addContact a el viewModel

--- app/src/main/java/com/ebc/contactos_app/viewmodel/ContactViewModel.kt
+++ app/src/main/java/com/ebc/contactos_app/viewmodel/ContactViewModel.kt
@@ -4,11 +4,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ebc.contactos_app.dao.ContactDao
 import com.ebc.contactos_app.enity.Contact
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class ContactViewModel(private val dao: ContactDao): ViewModel() {
     val contacts: StateFlow<List<Contact>> = dao.getAll()
         .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+
+
+    fun addContact(name: String, phone: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val myContact = Contact(name = name, phone = phone)
+            dao.insert(myContact)
+        }
+
+    }
 }
